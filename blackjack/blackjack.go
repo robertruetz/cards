5a079718a1679df47ccc2d60ebc1f6f167861fa4
robert ruetz <ruetz.robert@gmail.com>
2022-04-03 22:00:51 -0500

    more work on the blackjack impl. cleaned up output of hands and refactored the play loop to deal more correctly.

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -1,51 +1,76 @@
 package blackjack
 
 import (
-	"errors"
 	"fmt"
 	cards "github.com/robertruetz/deckOfCards/pkg"
-	"github.com/robertruetz/deckOfCards/util"
+	"strings"
 )
 
-const InitHandSize = 2
-
-type Player struct {
-	hand []cards.Card
+type Hand struct {
+	downCard cards.Card
+	upCards []cards.Card
 }
 
-func NewPlayer() *Player {
+func NewHand(down cards.Card, up cards.Card) Hand {
+	down.FaceUp = false
+	up.FaceUp = true
+	h := Hand{
+		downCard: down,
+		upCards: []cards.Card{ up },
+	}
+	return h
+}
+
+type Player struct {
+	hand Hand
+}
+
+func (p *Player) PrintHand() string {
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("\n\tDown: %s\n", p.hand.downCard))
+	sb.WriteString("\tUp: ")
+	for i, c := range p.hand.upCards {
+		sb.WriteString(c.String())
+		if i + 1 < len(p.hand.upCards) {
+			sb.WriteString(", ")
+		}
+	}
+	return sb.String()
+}
+
+func NewPlayer(hand Hand) *Player {
 	p := Player{
-		hand: make([]cards.Card, 0, InitHandSize),
+		hand: hand,
 	}
 	return &p
 }
 
-func (p *Player) DealCard(card cards.Card) error {
-	if len(p.hand) < InitHandSize {
-		p.hand = append(p.hand, card)
-		return nil
+func (p *Player) DealCard(card cards.Card) {
+	if p.hand.upCards == nil {
+		p.hand.upCards = make([]cards.Card, 0)
 	}
-	return errors.New("hand is full")
+	p.hand.upCards = append(p.hand.upCards, card)
 }
 
 func Play(){
-	deck := cards.NewDeckOfCards()
+	deck := cards.NewCardDeck()
 	deck.Shuffle()
-	p1 := NewPlayer()
-	p2 := NewPlayer()
-	for i := 0; i < InitHandSize; i++ {
-		err := p1.DealCard(*deck.Deal())
-		if err != nil {
-			panic(err.Error())
-		}
-		err = p2.DealCard(*deck.Deal())
-		if err != nil {
-			panic(err.Error())
-		}
-	}
-	fmt.Printf("Player 1 hand: %s\n Player 2 hand: %s\n", util.JSONify(p1.hand), util.JSONify(p2.hand))
+	p1 := NewPlayer(NewHand(*deck.Deal(false), *deck.Deal(true)))
+	dealer := NewPlayer(NewHand(*deck.Deal(false), *deck.Deal(true)))
+
+	p1.DealCard(*deck.Deal(true))
+
+	fmt.Printf("Player 1 hand: %s\nDealer hand: %s\n", p1.PrintHand(), dealer.PrintHand())
 	// TODO:
 		// decision p1
-		// decision p2
+		// decision dealer
 		// resolve game
 }
+
+/* Dealer Rules
+When the dealer has served every player, the dealers face-down card is turned up.
+If the total is 17 or more, it must stand.
+If the total is 16 or under, they must take a card.
+The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand.
+If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21),the dealer must count the ace as 11 and stand.
+ */
--- pkg/deck.go
+++ pkg/deck.go
@@ -1,7 +1,9 @@
 package pkg
 
 import (
+	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -11,17 +13,25 @@ type Suit string
 
 const (
 	DIAMONDS Suit = "DIAMONDS"
-	CLUBS = "CLUBS"
-	SPADES = "SPADES"
-	HEARTS = "HEARTS"
+	CLUBS Suit = "CLUBS"
+	SPADES Suit = "SPADES"
+	HEARTS Suit = "HEARTS"
 )
+
+var symbols map[string]rune = map[string]rune{
+	"SPADES": '♠',
+	"DIAMONDS": '♦',
+	"HEARTS": '♥',
+	"CLUBS": '♣',
+}
 
 type CardDeck struct {
 	cards []Card
 	currentCard int
 }
 
-func NewDeckOfCards() CardDeck {
+// NewCardDeck returns an ordered deck of playing cards.
+func NewCardDeck() CardDeck {
 	d := CardDeck{
 		cards: make([]Card, TotalCards),
 	}
@@ -36,13 +46,15 @@ func NewDeckOfCards() CardDeck {
 	return d
 }
 
+// swap replaces the value at pos1 with the value at pos2 and vice versa.
 func (d *CardDeck) swap(pos1 int, pos2 int) {
 	hold := d.cards[pos1]
 	d.cards[pos1] = d.cards[pos2]
 	d.cards[pos2] = hold
 }
 
-// Shuffle swaps the card at each position in the deck with a random card from the remaining positions.
+// Shuffle swaps the card at each position in the deck with a random card from the remaining possible positions.
+// Simulates shuffling a deck of cards
 func (d *CardDeck) Shuffle() {
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < TotalCards; i++ {
@@ -53,10 +65,11 @@ func (d *CardDeck) Shuffle() {
 
 // Deal returns the card at the index currentCard for the given deck.
 // Nil is returned if the currentCard is greater than TotalCards signifying an empty deck
-func (d *CardDeck) Deal() *Card {
+func (d *CardDeck) Deal(faceUp bool) *Card {
 	var c *Card
 	if d.currentCard < TotalCards {
 		c = &d.cards[d.currentCard]
+		c.FaceUp = faceUp
 		d.currentCard++
 	}
 	return c
@@ -65,12 +78,35 @@ func (d *CardDeck) Deal() *Card {
 type Card struct {
 	Suit Suit
 	Rank int
+	FaceUp bool
+}
+
+func (c Card) String() string {
+	return fmt.Sprintf("%v of %c", c.rankSwap(), symbols[string(c.Suit)])
 }
 
 func NewCard(suit Suit, rank int) Card {
 	return Card{
 		Suit: suit,
 		Rank: rank,
+		FaceUp: false,
+	}
+}
+
+// rankSwap swaps out the Rank of the given card for the appropriate string value.
+// Handles Ace, Jack, Queen, King
+func (c Card) rankSwap() string {
+	switch c.Rank {
+	case 1:
+		return "Ace"
+	case 11:
+		return "Jack"
+	case 12:
+		return "Queen"
+	case 13:
+		return "King"
+	default:
+		return strconv.Itoa(c.Rank)
 	}
 }
 
